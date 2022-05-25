45bfc6e43d242040662ed6420a5d10c2bf949741
keremsahiin <1910205057@ogrenci.karabuk.edu.tr>
2022-05-25 23:49:56 +0300

    Show Products on the Home Page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -36,13 +36,14 @@
     <div class="about">
         <div class="container">
             <div class="row align-items-center">
+                @foreach($contentlist1 as $rs)
                 <div class="col-lg-6">
-                    <div class="about-img" data-parallax="scroll" data-image-src="{{asset('assets')}}/img/about.jpg"></div>
+                    <div class="about-img" data-parallax="scroll" data-image-src="{{Storage::url($rs->image)}}" style="height: 169px; width: 299px"></div>
                 </div>
                 <div class="col-lg-6">
                     <div class="section-header">
-                        <p>Learn About Us</p>
-                        <h2>Worldwide non-profit charity organization</h2>
+                        <p>{{$rs->title}}</p>
+                        <h2>{{$rs->title}}</h2>
                     </div>
                     <div class="about-tab">
                         <ul class="nav nav-pills nav-justified">
@@ -59,17 +60,18 @@
 
                         <div class="tab-content">
                             <div id="tab-content-1" class="container tab-pane active">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae pellentesque turpis. Donec in hendrerit dui, vel blandit massa. Ut vestibulum suscipit cursus. Cras quis porta nulla, ut placerat risus. Aliquam nec magna eget velit luctus dictum. Phasellus et felis sed purus tristique dignissim. Morbi sit amet leo at purus accumsan pellentesque. Vivamus fermentum nisi vel dapibus blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                {{$rs->detail}}
                             </div>
                             <div id="tab-content-2" class="container tab-pane fade">
-                                Sed tincidunt, magna ut vehicula volutpat, turpis diam condimentum justo, posuere congue turpis massa in mi. Proin ornare at massa at fermentum. Nunc aliquet sed nisi iaculis ornare. Nam semper tortor eget est egestas, eu sagittis nunc sodales. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent bibendum sapien sed purus molestie malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                {{$rs->detail}}
                             </div>
                             <div id="tab-content-3" class="container tab-pane fade">
-                                Aliquam dolor odio, mollis sed feugiat sit amet, feugiat ut sapien. Nunc eu dignissim lorem. Suspendisse at hendrerit enim. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed condimentum semper turpis vel facilisis. Nunc vel faucibus orci. Mauris ut mauris rhoncus, efficitur nisi at, venenatis quam. Praesent egestas pretium enim sit amet finibus. Curabitur at erat molestie, tincidunt lorem eget, consequat ligula.
+                                {{$rs->detail}}
                             </div>
                         </div>
                     </div>
                 </div>
+                @endforeach
             </div>
         </div>
     </div>
