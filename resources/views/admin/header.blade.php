89b2e060c51a8182821a355b216d8c5027c180fe
keremsahiin <1910205057@ogrenci.karabuk.edu.tr>
2022-06-12 01:14:43 +0300

    Admin Panel Login Form & Login

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -10,7 +10,7 @@
                     <ul class=" navbar-right">
                         <li class="nav-item dropdown open" style="padding-left: 15px;">
                             <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                                <img src="{{asset('assets')}}/admin/images/img.jpg" alt="">John Doe
+                                <img src="{{asset('assets')}}/admin/images/img.jpg" alt="">{{ Auth::user()->name }}
                             </a>
                             <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item"  href="javascript:;"> Profile</a>
@@ -19,7 +19,7 @@
                                     <span>Settings</span>
                                 </a>
                                 <a class="dropdown-item"  href="javascript:;">Help</a>
-                                <a class="dropdown-item"  href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                                <a class="dropdown-item"  href="/logoutuser"><i class="fa fa-sign-out pull-right"></i> LogOut</a>
                             </div>
                         </li>
 
